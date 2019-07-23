351209060822fb615e80507774a2d6aacd30a339
luis perez <luis.contreras@mipc.com.mx>
2019-07-23 11:08:34 -0500

    Write file

---
+++ 03_base_node/app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+let base = 2;
+let data = '';
+
+for (let i = 1; i <= 10; i++) {
+  data += `${base} * ${i} = ${base * i} \n`;
+}
+
+fs.writeFile('tablas.txt',data,(err) =>{
+
+  if(err){
+    throw  err;
+  }
+
+  console.log("Archivo creado con exito");
+});
